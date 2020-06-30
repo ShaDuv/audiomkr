c82ea764ebfe295f674c645a55e07620e92ac8bd
Shawna Duvall <shawnaduvall@gmail.com@>
2020-06-29 23:25:41 -0700

    Adds Conditional Logic to Populate Array

--- audiomkr.rb
+++ audiomkr.rb
@@ -3,4 +3,12 @@
 require "wavefile"
 include WaveFile
 
+x = 0
+FILES_TO_APPEND = []
+until x >= 6 do
+  rand_num = rand(1..6)
+  reader = Reader.new("#{rand_num}.wav")
+  x = x + reader.total_duration.seconds
+  FILES_TO_APPEND.push("#{rand_num}.wav")
+end
 
