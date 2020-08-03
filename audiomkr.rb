c7b22c9a2ab962d5ecb2deeda693a6d6e0a20a72
Shawna Duvall <shawnaduvall@gmail.com@>
2020-08-03 00:12:57 -0700

    prevent sequential number duplication

--- audiomkr.rb
+++ audiomkr.rb
@@ -2,22 +2,33 @@
 
 require "wavefile"
 include WaveFile
+
 puts "What is the topic?"
 topic = gets.chomp
+
 puts "How many minutes?"
 time = gets.chomp
+
+puts "What do you want to call the final file?"
+name = gets.chomp
+
+save_as = "#{name}.wav"
+previous = 0
 x = 0
 count = Dir["#{topic}//**/*"].length
-
 FILES_TO_APPEND = []
-until x >= time.to_i * 60 do
+
+until x >= time.to_i  do
   rand_num = rand(1..count)
-  reader = Reader.new("#{topic}/#{rand_num}.wav")
-  x = x + reader.total_duration.seconds
-  FILES_TO_APPEND.push("#{topic}/#{rand_num}.wav")
+  if rand_num != previous
+    reader = Reader.new("#{topic}/#{rand_num}.wav")
+    x = x + reader.total_duration.seconds
+    FILES_TO_APPEND.push("#{topic}/#{rand_num}.wav")
+    previous = rand_num
+  end 
 end
 
-Writer.new("generated_track.wav", Format.new(:stereo, :pcm_24, 44100)) do |writer|
+Writer.new(save_as, Format.new(:stereo, :pcm_24, 44100)) do |writer|
    FILES_TO_APPEND.each do |file_name|
        Reader.new(file_name).each_buffer do |buffer|
       writer.write(buffer)
@@ -25,4 +36,4 @@ Writer.new("generated_track.wav", Format.new(:stereo, :pcm_24, 44100)) do |write
   end
 end
 
-p count
+
